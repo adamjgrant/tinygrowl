c87742decedfb6bf1feb0001df980cfb37242451
Adam Kochanowicz <ajkochanowicz@gmail.com>
2013-03-14 16:41:27 -0400

    updated with tests...which are failing

--- tinygrowl.js
+++ tinygrowl.js
@@ -1,39 +1,56 @@
-// Growl Style
-// Just to be an asshole, let's define the css in JavaScript.
-var gStyle = {
-  box: 'margin-bottom: 20px; padding: 0px 15px; background-color: #FFFBCF; border: 1px solid #FDF0AA;'
-  , container: 'position: absolute; top: 20px; right: 20px; width: 200px; '
-  , p: 'font-family: helvetica neue, helvetica, sans-serif; font-size 11pt; line-height: normal; margin-bottom: 15px;'
-}
+var growl = function(opts) {
+  // OPTS
+  // ====
+  //
+  // REQUIRED 
+  // --------
+  // text: The html/text content to show under the title or by itself.
+  //
+  // OPTIONAL
+  // --------
+  // title: Bold-faced html/text content to appear to the left of the text.
+  // type: Either success/warning/info/error (Info is default)
+  // delay: Milliseconds before growl disappears. (0 is default and creates
+  // a dismissable growl.)
+  // container: css-formatted (e.g. ".container") container in which to
+  // populate growls. Default is "body"
 
-var styles = document.createElement('style')
-styles.innerText = '#growlContainer p,a,em,strong,li{' + gStyle.p + '}'
-styles.innerText += '#growlContainer > .look-up{' + gStyle.box + '}'
-styles.innerText += '#growlContainer{' + gStyle.container + '}'
-document.body.appendChild(styles)
+  // EXTEND
+  this.opts = opts
 
-// Growl Object
-var growl = function(msg, delay) {
-  var item = function(msg) {
-    this.identifier = Math.floor(Math.random()*1000)
-    this.messageBox = document.createElement('div')
-    this.messageBox.className = 'look-up ' + this.identifier
-    this.messageBox.setAttribute('aria-role','dialog')
-    $(this.messageBox).html(
-      '<p>' + msg + '</p>' 
-    )
-    return this;
+  // FALLBACKS
+  var _ = this.opts
+  _.type = ( opts.type || 'info')
+  _.delay = ( opts.delay || 0 )
+  _.container = ( opts.container || 'body')
+
+  // FORMATTING
+  _.fTitle =  ( _.title ? '<strong>' + _.title + '</strong>' : '')
+
+  // UNIQUE IDENTIFICATION
+  this.id = Math.floor(Math.random()*1000)
+
+  // CREATE FULL DOM OBJECT
+  this.html = document.createElement('div')
+  this.html.className = 'growl' + this.id + ' alert alert-' + _.type
+  this.html.setAttribute( 'aria-role', 'dialog' )
+  var xHtml = '<a class="close" data-dismiss="alert" href="#">&times;</a>'
+  $(this.html).html( 
+    (_.delay == 0 ? xHtml : '') 
+    + _.fTitle + ' ' + _.text
+  )
+
+  // APPEND ALERT TO CONTAINER
+  $(_.container).append(this.html)
+
+  // SET DELAYS
+  if ( _.delay > 0 ) { 
+    var fn = (function(delay, id) { 
+      setTimeout(function() {
+        $('.growl' + id).fadeOut('slow')
+        }
+        , delay
+      )
+    })(_.delay, this.id) 
   }
-  var growlContainer = document.createElement('div')
-  growlContainer.id = "growlContainer"
-  var growlMessage = item(msg)
-  $('body').append(growlContainer)
-  $('#growlContainer').append(growlMessage['messageBox'])
-  var fn = (function(delay, id) { 
-    setTimeout(function() {
-        $('.look-up.' + id).fadeOut('slow')
-      }
-      , (delay || 2000)
-    )
-  })(delay, growlMessage['identifier'])
-}
+} 
